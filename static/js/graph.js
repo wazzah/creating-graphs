72e5c1d4748ffd7fd98fdda842b4201bb4c4ec33
Warren Howes <warren.howes@hotmail.co.uk>
2019-04-12 01:01:18 +0000

    initial commit

---
+++ static/js/graph.js
@@ -0,0 +1,68 @@
+queue()
+    .defer(d3.csv, "data/Salaries.csv")
+    .await(makeGraphs);
+    
+function makeGraphs(error, salaryData) {
+    var ndx = crossfilter(salaryData);
+    
+    show_discipline_selector(ndx);
+    show_gender_balance(ndx);
+    show_average_salaries(ndx);
+    
+    dc.renderAll();
+}
+
+function show_discipline_selector(ndx) {
+    var dim = ndx.dimension(dc.pluck('discipline'));
+    var group = dim.group();
+    
+    dc.selectMenu("#discipline-selector")
+        .dimension(dim)
+        .group(group);
+}
+
+function show_gender_balance(ndx) {
+    var dim = ndx.dimension(dc.pluck('sex'));
+    var group = dim.group();
+    
+    dc.barChart("#gender-balance")
+        .width(400)
+        .height(300)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Gender")
+        .yAxis().ticks(20);
+}
+
+function show_average_salaries(ndx) {
+    var dim = ndx.dimension(dc.pluck('sex'));
+
+    function add_item(p, v) {
+        p.count++;
+        p.total += y.salary;
+        p.average = p.total;
+        return p;
+    }
+
+    function remove_item(p, v) {
+        p.count--;
+        if (p.count == 0) {
+            p.total = 0;
+            p.average = 0;
+        } else {
+            p.total -= v.salary;
+            p.average = p.total / p.count;
+        }
+        return p;
+    }
+
+    function initialise () {
+        return {count: 0, total: 0, average: 0};
+    }
+
+    var averageSalaryByGender = dim.group().reduce(add_item, remove_item, initialise);
+}
